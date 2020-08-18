cccd8708755fc8be9b799d7cb3408cd38dd4e777
CallMeTrebor <60898829+CallMeTrebor@users.noreply.github.com>
2020-08-18 09:32:08 +0300

    AudioManager Added

---
+++ Assets/Scripts/Audio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Audio
+{
+
+	public AudioClip clip;
+
+	public string name;
+
+	[Range(0f, 1.0f)]
+	public float volume;
+
+	[Range(.1f, 3.0f)]
+	public float pitch;
+
+	[HideInInspector]
+	public AudioSource source;
+}
---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    public Audio[] sounds;
+    void Awake()
+    {
+        foreach (Audio sound in sounds)
+        {
+            sound.source = gameObject.AddComponent<AudioSource>();
+            sound.source.clip = sound.clip;
+
+            sound.source.volume = sound.volume;
+            sound.source.pitch = sound.pitch;
+        }
+    }
+
+    public void play(string index)
+    {
+        Audio s = Array.Find(sounds, sound => sound.name == index);
+        s.source.Play();
+    }
+}
